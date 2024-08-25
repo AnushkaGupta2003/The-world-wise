91b485937ae4705cc73aef722750c5daa4858018
Anushka Gupta <121218617+AnushkaGupta2003@users.noreply.github.com>
2024-08-25 22:45:29 +0530

    Delete src directory

--- src/components/NavBar/NumResults/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-//Presentational Component
-const NumResults = ({ movies })=>
-{
-  return (<p className="num-results">Found <strong>{ movies?.length }</strong> results</p>);
-}
-
-export default NumResults;
